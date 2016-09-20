82117b2be4762d084b1d0a63f4f2b73d4b888a39
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-09-20 21:39:46 +0300

    deleted model1

--- LocalDB/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [FirstName] NCHAR(10) NULL, 
-    [LastName] NCHAR(10) NULL, 
-    [ThirdName] NCHAR(10) NULL, 
-    [Email] NCHAR(10) NULL
-)
