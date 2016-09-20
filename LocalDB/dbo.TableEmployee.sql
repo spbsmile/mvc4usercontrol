da870a57eb810eedc521d04c2f3236d66ec4293c
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-09-20 21:43:22 +0300

    sql tableEmployee

--- LocalDB/dbo.TableEmployee.sql
+++ LocalDB/dbo.TableEmployee.sql
@@ -1,8 +1,4 @@
-﻿CREATE TABLE [dbo].[Table]
+﻿CREATE TABLE [dbo].[TableEmployee]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [FirstName] NCHAR(10) NULL, 
-    [LastName] NCHAR(10) NULL, 
-    [ThirdName] NCHAR(10) NULL, 
-    [Email] NCHAR(10) NULL
+	[Id] INT NOT NULL PRIMARY KEY
 )
